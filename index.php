4ad88a621be4b1dc5551f62f19675acf84806902
anuradha <astroanu2004@gmail.com>
2013-07-28 16:42:33 +0530

    added exception class

--- index.php
+++ index.php
@@ -3,21 +3,22 @@
 require_once '/lib/LiveApiClient.php';
 require_once '/config.inc.php';
 
-
-
 $live = new LiveAPIClient(LIVE_ID, LIVE_SEC, 'http://www.liveapi/');
 
-if(!$live->fetchAccessToken()){
+
+if(!$live->fetchAccessToken() && !isset($_COOKIE['livetoken'])){
 	$live->authorize();
-}else{
-	//print_r($live->fetchAccessToken());
-	
-	$res = $live->request('https://apis.live.net/v5.0/me/contactsa', 'GET');
+}
+elseif(is_object($live->fetchAccessToken())){
+	setcookie('livetoken', $live->getAccessToken(true));
+	echo 'connected';
+	header('Location: '. $_SERVER['PHP_SELF']);
+}
+elseif(isset($_COOKIE['livetoken']) && is_object(json_decode($_COOKIE['livetoken']))){
+	echo 'token read from cookie';
+	$live->setAccessToken($_COOKIE['livetoken']);
+	$res = $live->request('https://apis.live.net/v5.0/me/contacts', 'GET');
 	print_r($res);
 }
-
-//print_r($live->getAccessToken());
-//echo $_GET['code'];
-
 
 ?>
--- lib/LiveAPIClient.php
+++ lib/LiveAPIClient.php
@@ -6,12 +6,14 @@ class LiveAPIClient{
 	protected $_redirect_url;
 	protected $_access_token;
 	protected $_scope;
+	protected $_debug;
 	
-	function __construct($liveId, $liveSecret, $redirectUrl){
+	function __construct($liveId, $liveSecret, $redirectUrl, $debug = true){
 		$this->setLiveId($liveId) ;
 		$this->setLiveSecret($liveSecret);
 		$this->setRedirectUrl($redirectUrl);
 		$this->setScopes('wl.basic,wl.contacts_emails');
+		$this->_debug = $debug;
 	}
 	
 	public function fetchAccessToken(){
@@ -22,26 +24,41 @@ class LiveAPIClient{
 			'client_secret' => $this->getLiveSecret(),
 			'redirect_uri' => $this->getRedirectUrl(),				
 		);
-		
-		$ch = curl_init('https://login.live.com/oauth20_token.srf');		
-		$bodyData = http_build_query($postFields);		 
+		$bodyData = http_build_query($postFields);
 		$headers = array(
-			'Content-Type: application/x-www-form-urlencoded'
-		);		
+				'Content-Type: application/x-www-form-urlencoded'
+		);
 		
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $bodyData);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		 
-		$req = curl_exec($ch);		
-		
-		if(is_object($token = json_decode($req)) && property_exists($token, 'access_token')){
-			$this->_access_token = $token->access_token;
-			return $token;
+		try{
+			$ch = curl_init('https://login.live.com/oauth20_token.srf');
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $bodyData);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);				
+			$req = curl_exec($ch);
+			
+			if(is_object($responseObj = json_decode($req)) && property_exists($responseObj, 'access_token')){
+				throw new LiveAPIException('ok', 200);
+			}elseif(is_object($responseObj = json_decode($req)) && property_exists($responseObj, 'error')){
+				throw new LiveAPIException($responseObj, 1001);
+			}else{
+				throw new LiveAPIException('Unknown Live API Exception', 1000);
+			}			
+		}
+		catch (LiveAPIException $e){
+			switch ($e->getCode()) {
+				case 200:
+					$responseObj->ts = time();
+					$this->setAccessToken(json_encode($responseObj));
+					return $this->getAccessToken();
+					break;
+				
+				default:
+					return false;
+					break;
+			}
 		}		
-		return false;
 	}
 	
 	public function authorize($returnUrl = false){
@@ -54,26 +71,46 @@ class LiveAPIClient{
 	} 
 	
 	public function request($url, $method = 'GET', $params = array()) {
+		if(intval($this->getAccessToken('ts')) + intval($this->getAccessToken('expires_in')) < time()){
+			echo 'expired';
+		}else{
+			echo intval($this->getAccessToken('expires_in'));
+			echo 'still valid:will expire in '.date('Y d m - h i s', intval($this->getAccessToken('ts')) + intval($this->getAccessToken('expires_in')));
+		}
+		
 		$postFields = array(
 			'client_id' => $this->_live_id,
 			'client_secret' => $this->_live_secret
 		);
 		
 	    $bodyData = http_build_query($postFields);	    
-	    $headers = array('Authorization: Bearer ' . $this->_access_token);
-	    $ch = curl_init($url .'?'. http_build_query($params));
+	    $headers = array('Authorization: Bearer ' . $this->getAccessToken('access_token'));
 	    
-	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-	    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	    
-	    $response = curl_exec($ch);
-	    
-	  	if(is_object($responseObj = json_decode($response))){
-	  		return $responseObj;
-	  	}
-		return false;	    
+	    try{
+	    	$ch = curl_init($url .'?'. http_build_query($params));	    	 
+	    	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+	    	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);	    	 
+	    	$response = curl_exec($ch);	    
+
+	    	if(is_object($responseObj = json_decode($response)) && property_exists($responseObj, 'error')){
+	    		throw new LiveAPIException($responseObj, 1000);
+	    	}else{
+	    		throw new LiveAPIException('ok', 200);
+	    	}
+	    }
+	    catch(LiveAPIException $e){
+	    	switch ($e->getCode()) {
+	    		case 200:
+	    			return $responseObj;
+	    			break;
+	    		
+	    		default:
+	    			return $e;
+	    			break;
+	    	}
+	    }
 	}
 	
 	public function setLiveId($liveId){
@@ -93,11 +130,22 @@ class LiveAPIClient{
 	}
 
 	public function setAccessToken($accessToken){
+		if(is_string(func_get_args(0)) && is_object($tokenObj = json_decode($accessToken))){
+			$this->_access_token = $tokenObj;
+		}
 		$this->_access_token = $accessToken;
 	}
 	
 	public function getAccessToken(){
-		return $this->_access_token;
+		if(is_object($tokenObj = json_decode($this->_access_token))){
+			if(func_num_args() == 1 && is_string($portion = func_get_arg(0))){
+				return $tokenObj->$portion;
+			}elseif(func_num_args() == 1 && is_bool(func_get_arg(0)) && func_get_arg(0) == true){
+				return $this->_access_token;
+			}
+			return $tokenObj;
+		}
+		return null;
 	}	
 	
 	public function setRedirectUrl($redirectUrl){
@@ -111,7 +159,8 @@ class LiveAPIClient{
 	public function setScopes(){
 		if(is_array(func_get_arg(0))){
 			$this->_scope = implode(',', func_get_arg(0));
-		}elseif(is_string(func_get_arg(0))){
+		}
+		elseif(is_string(func_get_arg(0))){
 			$this->_scope = func_get_arg(0);
 		}
 	}
@@ -121,4 +170,24 @@ class LiveAPIClient{
 	}
 }
 
+class LiveAPIException extends Exception{
+	public function __construct($except, $code = 0, Exception $previous = null) {
+		if(is_object($except) && property_exists($except->error, 'code')){			
+			parent::__construct($except->error->code . ' : ' . $except->error->message, $code, $previous);
+		}
+		elseif(is_object($except) && property_exists($except, 'error')){
+			parent::__construct($except->error . ' : ' . $except->error_description, $code, $previous);
+		}
+		elseif(is_string($except)){
+			parent::__construct($except, $code, $previous);
+		}
+		else{
+			parent::__construct('Unknown Live API Error', 1001, $previous);
+		}
+	}
+	
+	public function __toString() {
+		return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+	}
+}
 ?>
